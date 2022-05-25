4592f8bf58131f39b8f10bcbea572fe617f09e1d
Pedro Chang <pedrochang91@gmail.com>
2022-05-24 21:25:29 -0700

    Completes the w04/Authentication

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,21 @@
+class UsersController < ApplicationController
+  def show
+    if current_user
+      render json: {message: "success"}
+    end
+  end
+
+  def create
+    user = User.new(
+      name: params[:name],
+      email: params[:email],
+      password: params[:password],
+      password_confirmation: params[:password_confirmation]
+    )
+    if user.save
+      render json: { message: "User created successfully" }, status: :created
+    else
+      render json: { errors: user.errors.full_messages }, status: :bad_request
+    end
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+  post "/users" => "users#create"
+  get "/users/:id" => "users#show"
+  
+  post "/sessions" => "sessions#create"
+end
